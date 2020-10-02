720219a171ed11dfb52104cd3281a14ad5e26282
Vasilis CodeX <vasilis.tris@gmail.com>
2020-10-02 18:43:22 +0300

    Started itineraries route & trips mock database

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -37,7 +37,7 @@
             $bananaLines = new BananaLines();
             $tripsResponse = $bananaLines->getTrips();
 
-            die($tripsResponse['data'][0]->getVesselName());
+            // die($tripsResponse['data'][0]->getVesselName());
 
             $response = new Response();
             $response->setContent( json_encode($tripsResponse) );
@@ -45,6 +45,54 @@
             $response->setStatusCode(Response::HTTP_OK);
             return $response;
         }
+
+        /**
+         * Returns the list of all available itineraries from Havana Ferries and Banana Lines
+         * @Route("/itineraries", name="itineraries_list")
+         */
+        public function getItineraries(Request $request) {
+            $trips = array();
+            $havanaFerries = new HavanaFerries();
+            $havanaFerriesTrips = $havanaFerries->getTrips();
+            if($havanaFerriesTrips['status']) $trips = array_merge($trips, $havanaFerriesTrips['data']);
+
+            $bananaLines = new BananaLines();
+            $bananaLinesTrips = $bananaLines->getTrips();
+            if($bananaLinesTrips['status']) $trips = array_merge($trips, $bananaLinesTrips['data']);
+
+            // There are trip instances.
+            if(!empty($trips)) {
+                $response = new Response();
+                $itinerariesResponse = array();
+                
+                foreach ($trips as $trip) {
+                    // echo $trip->getVesselName().'  |   ';
+
+                    // TODO: Fill all the below empty fields.
+                    $itinerariesResponse['itineraries'][] = array(
+                        "itineraryId"=>$trip->getItinerary(),
+                        "originPortCode"=>"The unique identifier for a port",
+                        "destinationPortCode"=>"The unique identifier for a port",
+                        "operatorCode"=>"The unique identifier for an operator",
+                        "operatorName"=>"The name of the operator",
+                        "vesselName"=>$trip->getVesselName(),
+                        "departureDateTime"=>$trip->getDepartureDate(),
+                        "arrivalDateTime"=>$trip->getArrivalDate()
+                    );
+
+                    
+                }
+
+                $response->setContent( json_encode($itinerariesResponse) );
+                $response->headers->set('Content-Type', 'application/json');
+                $response->setStatusCode(Response::HTTP_OK);
+                return $response;
+            } else {
+                // TODO: throw error
+            }
+
+        }
+
 
 
     }
