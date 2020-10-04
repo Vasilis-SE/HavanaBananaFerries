2191f124e883ea6d62b00173fb9ff37c56d08423
Vasilis CodeX <vasilis.tris@gmail.com>
2020-10-04 18:10:41 +0300

    Finished basic unit test for havana class endpoint bridge

--- tests/Model/HavanaFerriesTest.php
+++ tests/Model/HavanaFerriesTest.php
@@ -10,7 +10,7 @@
             $havanaFerries = new HavanaFerries();
             $response = $havanaFerries->getTrips();
 
-            $this->assertEquals(true, ($response['status'] && count($response['data']) > 0) );
+            $this->assertTrue(($response['status'] && count($response['data']) > 0));
         }
 
         public function testAPIURLIsCorrect() {
@@ -22,8 +22,53 @@
             $havanaFerries = new HavanaFerries();
             $havanaFerries->setBaseURL('https://fat3lw9sr6.execute-api.eu-west-3.googlecloud.com');
             $response = $havanaFerries->getTrips();
-            die($response);
-            $this->assertEquals(false, $response);
+            $this->assertFalse($response);
+        }
+
+        public function testFetchSpecificTrip() {
+            $havanaFerries = new HavanaFerries();
+            $response = $havanaFerries->getTrips(1);
+            $this->assertEquals(1, count($response['data']));
+        }
+
+        public function testTripInstanceIsCreatedCorrectly() {
+            $havanaFerries = new HavanaFerries();
+            $response = $havanaFerries->getTrips(1);
+            $trip = $response['data'][0];
+
+            $flag = true;
+            if(
+                $trip->getItinerary() == 0 ||
+                $trip->getVesselName() == '' ||
+                $trip->getDepartureDate() == '' ||
+                $trip->getArrivalDate() == '' ||
+                $trip->getCompanyPrefix() == '' ||
+                $trip->getCompanyName() == '' ||
+                $trip->getPortOrigin() == '' ||
+                $trip->getPortDestination() == ''
+            ) {
+                $flag = false;
+            }
+
+            $this->assertTrue($flag);
+        }
+
+        public function testInterfaceMethodsExist() {
+            $havanaFerries = new HavanaFerries();
+
+            $flag = true;
+            if(!method_exists($havanaFerries, 'getTrips')) $flag = false;
+            if(!method_exists($havanaFerries, 'getPrices')) $flag = false;
+            if(!method_exists($havanaFerries, 'convertToCents')) $flag = false;
+            if(!method_exists($havanaFerries, 'getBaseURL')) $flag = false;
+            if(!method_exists($havanaFerries, 'setBaseURL')) $flag = false;
+            
+            $this->assertTrue($flag);
+        }
+
+        public function testConversionToCents() {
+            $havanaFerries = new HavanaFerries();
+            $this->assertEquals(500, $havanaFerries->convertToCents(500));
         }
 
     }
